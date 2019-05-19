995fd607826e931dc76757343775041a52f6d6a3
alireza darbandi <darbandi1996@gmail.com>
2019-05-19 17:34:00 +0430

    fix in public html

--- public_html/index.php
+++ public_html/index.php
@@ -58,3 +58,8 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+
+// set the public path to this directory
+$app->bind('path.public', function() {
+  return __DIR__;
+});
